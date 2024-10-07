8ee18e8ef1ed492b43ae2dd32c53c3d942c8523e
TungNguyen12 <nvtung382@gmail.com>
2024-10-07 13:31:48 +0300

    create components of navigation bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,31 @@
 import { Nunito } from 'next/font/google'
 
-import type { Metadata } from "next";
-import "./globals.css";
-import Navbar from './components/navbar/Navbar';
-
+import type { Metadata } from 'next'
+import './globals.css'
+import Navbar from './components/navbar/Navbar'
+import Modal from './components/modals/Modal'
 
 export const metadata: Metadata = {
-  title: "Airbnb",
-  description: "Airbnb clone",
-};
+  title: 'Airbnb',
+  description: 'Airbnb clone',
+}
 
 const font = Nunito({
-  subsets: ["latin"],
+  subsets: ['latin'],
 })
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={font.className}
-      >
+    <html lang='en'>
+      <body className={font.className}>
+        <Modal actionLabel='Submit' title='AWS' isOpen />
         <Navbar />
         {children}
       </body>
     </html>
-  );
+  )
 }
